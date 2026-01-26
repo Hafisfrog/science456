2c04958b495ed72496422b564b882feda5974c2c
git branch -M main <hafismmrd@gmail.com>
2026-01-26 22:17:35 +0700

    index-p6

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-// import { useState } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Start from "./pages/Start";
 import SelectGrade from "./pages/SelectGrade";
+import Grade6 from "./pages/grade6";
 
 export default function App() {
   return (
@@ -11,11 +11,16 @@ export default function App() {
         <Route path="/grades" element={<SelectGrade />} />
 
         {/* เผื่อไว้ต่อยอด */}
-        <Route path="/p4" element={<div style={{padding:40}}>หน้า ป.4 (กำลังทำ)</div>} />
-        <Route path="/p5" element={<div style={{padding:40}}>หน้า ป.5 (กำลังทำ)</div>} />
-        <Route path="/p6" element={<div style={{padding:40}}>หน้า ป.6 (กำลังทำ)</div>} />
+        <Route
+          path="/p4"
+          element={<div style={{ padding: 40 }}>หน้า ป.4 (กำลังทำ)</div>}
+        />
+        <Route
+          path="/p5"
+          element={<div style={{ padding: 40 }}>หน้า ป.5 (กำลังทำ)</div>}
+        />
+        <Route path="/p6" element={<Grade6 />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
